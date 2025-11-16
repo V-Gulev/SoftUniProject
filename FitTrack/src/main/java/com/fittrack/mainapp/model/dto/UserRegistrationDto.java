80a0f418961ad333861a3070e9942604f85d30cd
V-Gulev <velizargulev@abv.bg>
2025-11-16 21:16:47 +0200

    feat: Add User Registration dto

---
+++ FitTrack/src/main/java/com/fittrack/mainapp/model/dto/UserRegistrationDto.java
@@ -0,0 +1,26 @@
+package com.fittrack.mainapp.model.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegistrationDto {
+
+    @NotEmpty(message = "Username cannot be empty.")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters.")
+    private String username;
+
+    @NotEmpty(message = "Email cannot be empty.")
+    @Email(message = "Please provide a valid email address.")
+    private String email;
+
+    @NotEmpty(message = "Password cannot be empty.")
+    @Size(min = 5, message = "Password must be at least 5 characters long.")
+    private String password;
+
+    private String confirmPassword;
+}
